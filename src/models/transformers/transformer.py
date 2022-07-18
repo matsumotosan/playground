cb7e4263404f2b0fb08045db0227c504c997b5b5
Shion <smatsumoto97@gmail.com>
2022-07-18 12:00:22 -0400

    Add positional encoding

--- src/models/transformers/transformer.py
+++ src/models/transformers/transformer.py
@@ -158,8 +158,18 @@ class Embeddings(nn.Module):
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, d_model, dropout, max_len=5000):
+        self.dropout = nn.Dropout(dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.aragnge(0, max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
 
     def forward(self):
-        pass
+        x = x + self.pe[:, :x.size(1)].requires_grad_(False)
+        return self.dropout(x)
