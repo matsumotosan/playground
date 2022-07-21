f795d9fa2049d4a34d581b7430f97e4e05575cd2
Shion <smatsumoto97@gmail.com>
2022-07-21 13:32:50 -0400

    EncoderDecoder transformer architecture completed

--- src/playground/models/transformers/transformer.py
+++ src/playground/models/transformers/transformer.py
@@ -57,12 +57,14 @@ class Transformer(pl.LightningModule):
             dropout,
             norm_first
         ) if decoder is None else decoder)
+        
+        self.initialize_weights()
     
     def encode(self, src, src_mask):
         return self.encoder(src, src_mask)
     
     def decode(self, tgt, memory, tgt_mask, memory_mask):
-        self.decoder(tgt, memory, tgt_mask, memory_mask)
+        return self.decoder(tgt, memory, tgt_mask, memory_mask)
     
     def forward(self, src, tgt, src_mask=None, tgt_mask=None, memory_mask=None):
         memory = self.encode(src, src_mask)
@@ -77,6 +79,15 @@ class Transformer(pl.LightningModule):
     
     def shared_step(self, batch, batch_idx):
         x, y = batch
+    
+    def initialize_weights(self, init_range=0.1):
+        pass
+        # for m in self.encoder.modules():
+        #     nn.init.uniform_(m.weight, -init_range, init_range)
+        #     nn.init.zeros_(m.bias) 
+        # for m in self.decoder.modules():
+        #     nn.init.uniform_(m.weight, -init_range, init_range) 
+        #     nn.init.zeros_(m.bias) 
     
     def configure_optimizers(self, beta1=0.9, beta2=0.98):
         optimizer = torch.optim.Adam(
@@ -100,7 +111,7 @@ class Transformer(pl.LightningModule):
     
     @staticmethod
     def _get_decoder(d_model, n_head, n_decoder_layers, dim_feedforward, dropout, norm_first):
-        decoder_layer = TransformerEncoderLayer(
+        decoder_layer = TransformerDecoderLayer(
             d_model,
             n_head,
             dim_feedforward,
@@ -109,7 +120,15 @@ class Transformer(pl.LightningModule):
         )
         decoder = TransformerDecoder(decoder_layer, n_decoder_layers)
         return decoder
-        
+    
+    @staticmethod
+    def generate_subsequent_mask(size):
+        """Return mask for output embeddings"""
+        attn_shape = (size, size)
+        subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1).type(torch.uint8)
+        return subsequent_mask == 0
+
+
 class TransformerEncoder(nn.Module):
     """TransformerEncoder consists of a stack of N TransformerEncoderLayer layers."""
     def __init__(self, encoder_layer, n_layers):
@@ -182,7 +201,7 @@ class TransformerEncoderLayer(nn.Module):
             x = x + self._ff_block(self.norm2(x))
         else:
             x = self.norm1(x + self._sa_block(x, mask))
-            x = self.norm2(x + self._ff_block())
+            x = self.norm2(x + self._ff_block(x))
             
         return x
     
@@ -213,9 +232,24 @@ class TransformerDecoderLayer(nn.Module):
         self.dropout3 = nn.Dropout(dropout)
         self.norm_first = norm_first
         
-    def forward(self, tgt, memory, tgt_mask, memory_mask):
+    def forward(self, tgt, memory, tgt_mask=None, memory_mask=None):
         """Forward pass through a decoder layer. Provides option to perform LayerNorm operation 
-        before or after self-attention, multi-head attention, and feed forward operations."""
+        before or after self-attention, multi-head attention, and feed forward operations.
+        
+        Parameters
+        ----------
+        tgt : 
+            Decoder input
+        
+        memory :
+            Output of last encoder layer
+        
+        tgt_mask : 
+            Mask for decoder input
+        
+        memory_mask : 
+            Mask for outout of last encoder layer
+        """
         x = tgt
         if self.norm_first:
             x = x + self._sa_block(self.norm1(x), tgt_mask)
@@ -223,7 +257,7 @@ class TransformerDecoderLayer(nn.Module):
             x = x + self._ff_block(self.norm3(x))
         else:
             x = self.norm1(x + self._sa_block(x, tgt_mask))
-            x = self.norm2(x + self.mha_block(x, memory, memory_mask))
+            x = self.norm2(x + self._mha_block(x, memory, memory_mask))
             x = self.norm3(x + self._ff_block(x))
             
         return x
@@ -260,7 +294,10 @@ class SelfAttention(nn.Module):
         self.fc_out = nn.Linear(d_model, d_model)
     
     def forward(self, values, keys, queries, mask=None):
-        """Performs self-attention operation
+        """Performs self-attention operation.
+        
+        Arguments are ordered as (V, K, Q) as opposed to (Q, K, V) to be consistent with the 
+        'Multi-Head Attention' schematic (Fig. 2 (right) from Vaswani et al., 2017).
         
         Parameters
         ----------
@@ -270,8 +307,8 @@ class SelfAttention(nn.Module):
         
         queries : Tensor of shape (N, q, )
         
-        mask : 
-            Mask, default=None
+        mask : Tensor of shape () 
+            Attention mask, default=None
         """
         # Get dimensions of values, keys, and queries
         N = queries.shape[0]
@@ -371,10 +408,3 @@ class PositionalEncoding(nn.Module):
 def _clone_layer(layer, N):
     """Returns a ModuleList of N identical modules."""
     return nn.ModuleList([copy.deepcopy(layer) for _ in range(N)])
-
-
-def generate_subsequent_mask(size):
-    """Return mask for output embeddings"""
-    attn_shape = (size, size)
-    subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1).type(torch.uint8)
-    return subsequent_mask == 0
