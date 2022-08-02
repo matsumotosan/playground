14e23b6ff66228cce7359b0fc9ad9b9d512bacf3
Shion <smatsumoto97@gmail.com>
2022-08-02 01:24:36 -0400

    Add SVHN DataModule

--- playground/datamodules/__init__.py
+++ playground/datamodules/__init__.py
@@ -1,5 +1,8 @@
 from .mnist import MNISTDataModule
+from .svhn import SVHNDataModule
+
 
 __all__ = [
     "MNISTDataModule",
+    "SVHNDataModule"
 ]
--- playground/datamodules/svhn.py
+++ playground/datamodules/svhn.py
@@ -1,10 +1,10 @@
 import pytorch_lightning as pl
 from torchvision import transforms
 from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import MNIST
+from torchvision.datasets import SVHN
 
 
-class MNISTDataModule(pl.LightningDataModule):
+class SVHNDataModule(pl.LightningDataModule):
     def __init__(self, data_dir, batch_size, num_workers, transform=None, split=0.8):
         super().__init__()
         self.data_dir = data_dir
@@ -20,28 +20,28 @@ class MNISTDataModule(pl.LightningDataModule):
             ])
 
     def prepare_data(self) -> None:
-        """Download MNIST data."""
-        MNIST(self.data_dir, train=True, download=True)
-        MNIST(self.data_dir, train=False, download=True)
+        """Download SVHN data."""
+        SVHN(self.data_dir, split='train', download=True)
+        SVHN(self.data_dir, split='test', download=True)
 
     def setup(self, stage=None) -> None:
         """Assign training, validation, and test data splits."""
         if stage == "fit" or stage is None:
-            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
-            self.mnist_train, self.mnist_val = random_split(
-                mnist_full, [int(self.split * len(mnist_full)), len(mnist_full) - int(self.split * len(mnist_full))]
+            svhn_full = SVHN(self.data_dir, train=True, transform=self.transform)
+            self.svhn_train, self.svhn_val = random_split(
+                svhn_full, [int(self.split * len(svhn_full)), len(svhn_full) - int(self.split * len(svhn_full))]
             )
         if stage == "test" or stage is None:
-            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
+            self.svhn_test = SVHN(self.data_dir, train=False, transform=self.transform)
 
     def train_dataloader(self) -> DataLoader:
         """Returns training DataLoader."""
-        return DataLoader(self.mnist_train, batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.svhn_train, batch_size=self.batch_size, num_workers=self.num_workers)
 
     def val_dataloader(self) -> DataLoader:
         """Returns validation DataLoader."""
-        return DataLoader(self.mnist_val, batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.svhn_val, batch_size=self.batch_size, num_workers=self.num_workers)
 
     def test_dataloader(self) -> DataLoader:
         """Returns testing DataLoader."""
-        return DataLoader(self.mnist_test, batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.svhn_test, batch_size=self.batch_size, num_workers=self.num_workers)
